596254a6507734d137ade878d5a43c0b78be3929
b1010119 <b1010119@fun.ac.jp>
2013-12-10 18:58:29 +0900

    add move triangle function

---
+++ static/js/colorpicker.js
@@ -0,0 +1,12 @@
+$(function(){
+	$("a data-wysihtml5-command").on("click", function(){
+		var colpik = getElementById("bg");
+		var colpik = true;
+		if(colpik == true){
+			
+			colpik = false;
+		}else{
+			colpik = true;
+		}
+	});
+});
