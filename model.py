2c9b7529e74cf1989ca3d042a45b2329181b1161
Toshihiro Kamiya <kamiya@mbj.nifty.com>
2013-12-19 16:11:20 +0900

    modify model and model's test cases

--- model.py
+++ model.py
@@ -27,6 +27,17 @@ class Group(object):
     def delete_all(clz, db):
         db.drop_collection("portfolio_groups")
 
+def delete_all_serial_numbers(db):
+    db.drop_collection("serial_number")
+
+def gen_serial_number(student_id, db):
+    col = db.serial_number
+    docs = col.find({"student_id": student_id})
+    docs = list(docs)
+    if not docs:
+        col.insert({"student_id": student_id, "seq": 0})
+    return col.find_and_modify({"student_id": student_id}, update={"$inc": {"seq": 1}}, new=True)["seq"]
+
 class UserDuplicationError(ValueError):
     pass
 
@@ -75,6 +86,7 @@ class User(object):
     @classmethod
     def delete_all(clz, db):
         db.drop_collection("portfolio_users")
+        delete_all_serial_numbers(db)
 
     @classmethod
     def find_user_ids_by_joining_group(clz, db, group_id):
@@ -87,156 +99,161 @@ class User(object):
                 store.append(doc["student_id"])
         return store
 
-class GoalDuplicationError(ValueError):
+class GoalInsertedTwice(ValueError):
     pass
 
 class Goal(object):
-    def __init__(self, student_id, title):
+    def __init__(self, student_id, title, serial=None):
         self.student_id = student_id
         self.title = title
-        
+        self.serial = serial
+
     def insert(self, db):
+        if self.serial is not None:
+            raise GoalInsertedTwice
+        serial = gen_serial_number(self.student_id, db)
         col = db.portfolio_goals
-        if Goal.find(db, self.student_id, self.title) != None:
-            raise GoalDuplicationError()
         col.insert({
             "student_id": self.student_id,
-            "title": self.title})
-            
+            "title": self.title,
+            "serial": serial})
+
+        # if insertion failed, will not reach here
+        self.serial = serial
+
     @classmethod
-    def find(clz , db, student_id, title):
+    def find(clz, db, student_id, serial):  # TODO API changed
         col = db.portfolio_goals
-        docs = col.find({"student_id": student_id, "title": title})
+        docs = col.find({"student_id": student_id, "serial": serial})
         docs = list(docs)
         if len(docs) == 0:
             return None
         doc = docs[0]
-        return Goal(doc["student_id"], doc["title"])
+        return Goal(doc["student_id"], doc["title"], doc["serial"])
 
     @classmethod
     def delete_all(clz, db):
         db.drop_collection("portfolio_goals")
-    
+
     @classmethod
     def get(clz, db, student_id):
         col = db.portfolio_goals
         docs = col.find({"student_id": student_id})
         docs = list(docs)
-        return [Goal(doc["student_id"], doc["title"]) for doc in docs] 
+        return [Goal(doc["student_id"], doc["title"], doc["serial"]) for doc in docs] 
 
     @classmethod
-    def remove(clz, db, student_id, title):
+    def remove(clz, db, student_id, serial):  # TODO API changed
         col = db.portfolio_goals
-        col.remove({"student_id": student_id, "title": title})
+        col.remove({"student_id": student_id, "serial": serial})
 
-
-class GoalItemDuplicationError(ValueError):
-    pass
-
-class GoalItemNotFoundError(ValueError):
+class GoalItemInsertedTwice(ValueError):
     pass
 
 class GoalItem(object):
-    def __init__(self, student_id, link_to_goal, title, change_data, visibility):
+    def __init__(self, student_id, goal_serial, title, change_data, visibility, serial=None):  # TODO API changed
         self.student_id = student_id
-        self.link_to_goal = link_to_goal
+        self.goal_serial = goal_serial
         self.title = title
         self.change_data = change_data 
-        self.visibility = visibility 
-    
+        self.visibility = visibility
+        self.serial = serial
+
     def insert(self, db):
+        if self.serial is not None:
+            raise GoalItemInsertedTwice
+        serial = gen_serial_number(self.student_id, db)
         col = db.portfolio_goal_items
-        if GoalItem.find(db, self.student_id,self.link_to_goal, self.title) != None:
-            raise GoalItemDuplicationError()
         col.insert({
             "student_id": self.student_id,
-            "link_to_goal": self.link_to_goal,
+            "goal_serial": self.goal_serial,
             "title": self.title,
             "change_data": self.change_data,
-            "visibility": self.visibility})
+            "visibility": self.visibility,
+            "serial": serial})
+
+        # if insertion failed, will not reach here
+        self.serial = serial
 
     def update(self, db):
-        GoalItem.remove(db, self.student_id, self.link_to_goal, self.title)
-        self.insert(db)
-    
+        assert self.serial  # has been inserted?
+        GoalItem.remove(db, self.student_id, self.goal_serial)
+        col = db.portfolio_goal_items
+        col.insert({
+            "student_id": self.student_id,
+            "goal_serial": self.goal_serial,
+            "title": self.title,
+            "change_data": self.change_data,
+            "visibility": self.visibility,
+            "serial": self.serial})
+
     @classmethod 
-    def find(clz , db, student_id, link_to_goal, title):
+    def find(clz, db, student_id, serial):  # TODO API changed
         col = db.portfolio_goal_items
         docs = col.find({
             "student_id": student_id, 
-            "link_to_goal": link_to_goal,
-            "title": title})
+            "serial": serial})
         docs = list(docs)
         if len(docs) == 0:
             return None
         doc = docs[0]
-        return GoalItem(doc["student_id"], doc["link_to_goal"], doc["title"], doc["change_data"], doc["visibility"])
+        return GoalItem(doc["student_id"], doc["goal_serial"], doc["title"], doc["change_data"], doc["visibility"], doc["serial"])
 
     @classmethod 
-    def get(clz , db, student_id, link_to_goal):
+    def get(clz , db, student_id, goal_serial):
         col = db.portfolio_goal_items
         docs = col.find({
             "student_id": student_id, 
-            "link_to_goal": link_to_goal})
+            "goal_serial": goal_serial})
         docs = list(docs)
-        return [GoalItem(doc["student_id"], doc["link_to_goal"], doc["title"], doc["change_data"], doc["visibility"]) for doc in docs]
-    
+        return [GoalItem(doc["student_id"], doc["goal_serial"], doc["title"], doc["change_data"], doc["visibility"], doc["serial"]) for doc in docs]
+
     @classmethod
-    def remove(clz, db, student_id, link_to_goal, title):
+    def remove(clz, db, student_id, serial):  # TODO API changed
         col = db.portfolio_goal_items
-        col.remove({"student_id": student_id, "link_to_goal": link_to_goal ,"title": title})
+        col.remove({"student_id": student_id, "serial": serial})
 
     @classmethod
     def delete_all(clz, db):
         db.drop_collection("portfolio_goal_items")
 
+class ItemLogInsertedTwice(ValueError):
+    pass
 
 class ItemLog(object):
-    def __init__(self, student_id, link_to_goal, link_to_goal_item, creation_date, text, number=None ):
+    def __init__(self, student_id, goalitem_serial, creation_date, text, serial=None):  # TODO API changed
         self.student_id = student_id
-        self.link_to_goal = link_to_goal
-        self.link_to_goal_item = link_to_goal_item
+        self.goalitem_serial = goalitem_serial
         self.creation_date = creation_date 
         self.text = text 
-        if number != None:
-            self.number = number
-        else :
-            self.number = None 
+        self.serial = serial
 
-
-    def insert(self, db):
+    def insert(self, db):  # TODO API changed
+        if self.serial is not None:
+            raise ItemLogInsertedTwice
+        serial = gen_serial_number(self.student_id, db)
         col = db.portfolio_item_logs
-        if self.number != None:
-            number = self.number
-        else :
-            docs = col.find({
-               "student_id" : self.student_id})
-            docs = list(docs)
-            max_number = 0
-            for doc in docs:
-                max_number = max(max_number, doc["number"])
-            number = max_number+1
-            
         col.insert({
             "student_id": self.student_id,
-            "link_to_goal": self.link_to_goal,
-            "link_to_goal_item": self.link_to_goal_item,
-            "number": number,
-            "creation_date" : self.creation_date,
-            "text" : self.text })
-        return number
+            "goalitem_serial": self.goalitem_serial,
+            "creation_date": self.creation_date,
+            "text": self.text,
+            "serial": serial })
+
+        # if insertion failed, will not reach here
+        self.serial = serial
 
     @classmethod
-    def find(clz, db, student_id, number):
+    def find(clz, db, student_id, serial):  # TODO API changed
         col = db.portfolio_item_logs
         docs = col.find({
             "student_id" : student_id,
-            "number" : number})
+            "serial" : serial})
         docs = list(docs)
         if len(docs) == 0:
             return None
         doc = docs[0]
-        return ItemLog(doc["student_id"], doc["link_to_goal"], doc["link_to_goal_item"], doc["number"], doc["creation_date"], doc["text"])
+        return ItemLog(doc["student_id"], doc["goalitem_serial"], doc["creation_date"], doc["text"], doc["serial"])
 
     @classmethod
     def get(clz, db, student_id):
@@ -245,13 +262,13 @@ class ItemLog(object):
         docs = list(docs)
         if len(docs) == 0:
             return None
-        else :
-            return [ItemLog(doc["student_id"], doc["link_to_goal"], doc["link_to_goal_item"], doc["number"], doc["creation_date"], doc["text"]) for doc in docs] 
+        else:
+            return [ItemLog(doc["student_id"], doc["goalitem_serial"], doc["creation_date"], doc["text"], doc["serial"]) for doc in docs] 
 
     @classmethod
-    def remove(clz, db, student_id, number):
+    def remove(clz, db, student_id, serial):  # TODO API changed
         col = db.portfolio_item_logs
-        col.remove({"student_id": student_id, "number": number})
+        col.remove({"student_id": student_id, "serial": serial})
 
     @classmethod
     def delete_all(clz, db):
@@ -260,36 +277,36 @@ class ItemLog(object):
 
 db = Connection('localhost', 27017).portbacker
 
-COL_GOALS = "goals"
-COL_PERSONALLOGS = "personallogs"
-
-def get_text_by_user_table_coumn(student_id, table, column):
-    col = db[table]
-    docs = col.find({"student_id": student_id})
-    texts = [doc.get(column) for doc in docs]
-    texts = list(filter(None, texts))
-    return texts
-
-def get_goal_texts(student_id):
-    goal_texts = get_text_by_user_table_coumn(student_id, COL_GOALS, "goal_text")
-    return goal_texts
-
-def remove_goal_text(student_id, goal_text):
-    col = db[COL_GOALS]
-    col.remove({"student_id": student_id, "goal_text": goal_text})
-
-def insert_goal_text(student_id, goal_text):
-    col = db[COL_GOALS]
-    col.insert({"student_id": student_id, "goal_text": goal_text})
-
-def get_log_texts(student_id):
-    log_texts = get_text_by_user_table_coumn(student_id, COL_PERSONALLOGS, "personallog_text")
-    return log_texts
-
-def remove_log_text(student_id, log_text):
-    col = db[COL_PERSONALLOGS]
-    col.remove({"student_id": student_id, "personallog_text": log_text})
-
-def insert_log_text(student_id, log_text):
-    col = db[COL_PERSONALLOGS]
-    col.insert({"student_id": student_id, "personallog_text": log_text})
+# COL_GOALS = "goals"
+# COL_PERSONALLOGS = "personallogs"
+# 
+# def get_text_by_user_table_coumn(student_id, table, column):
+#     col = db[table]
+#     docs = col.find({"student_id": student_id})
+#     texts = [doc.get(column) for doc in docs]
+#     texts = list(filter(None, texts))
+#     return texts
+# 
+# def get_goal_texts(student_id):
+#     goal_texts = get_text_by_user_table_coumn(student_id, COL_GOALS, "goal_text")
+#     return goal_texts
+# 
+# def remove_goal_text(student_id, goal_text):
+#     col = db[COL_GOALS]
+#     col.remove({"student_id": student_id, "goal_text": goal_text})
+# 
+# def insert_goal_text(student_id, goal_text):
+#     col = db[COL_GOALS]
+#     col.insert({"student_id": student_id, "goal_text": goal_text})
+# 
+# def get_log_texts(student_id):
+#     log_texts = get_text_by_user_table_coumn(student_id, COL_PERSONALLOGS, "personallog_text")
+#     return log_texts
+# 
+# def remove_log_text(student_id, log_text):
+#     col = db[COL_PERSONALLOGS]
+#     col.remove({"student_id": student_id, "personallog_text": log_text})
+# 
+# def insert_log_text(student_id, log_text):
+#     col = db[COL_PERSONALLOGS]
+#     col.insert({"student_id": student_id, "personallog_text": log_text})
