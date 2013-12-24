6586f9847fc37f77fc50e07fa5ab3d0a930c4835
Toshihiro Kamiya <kamiya@mbj.nifty.com>
2013-12-24 18:58:35 +0900

    keep goal-displaying state in goal page and personal-log page

--- static/js/chgbtn.js
+++ static/js/chgbtn.js
@@ -1,28 +1,39 @@
-$(function(){
-	var i = 1;
-	$('a.accordion-toggle').each(function(){
-		$(this).attr('id', 'goal-' + i);
-		++i;
-	});
-});
+var checkIdIsValid = function(word) {
+    return /^[A-Za-z][-A-Za-z0-9_:.]*$/.test(word);
+}
 
-var accordionState = [];
-$(function(){
-	$("a.accordion-toggle").on("click", function(){
-		var id = $(this).attr("id");
-		var state = accordionState[id];
-		if(accordionState[id] == undefined || ! accordionState[id]){
-			$("a.accordion-toggle").each(function(){
-				var oid = $(this).attr('id');
-				accordionState[oid] = false;
-				$("#" + oid + " img").css("transform", "rotate(0)");
-			});
-			$("#" + id + " img").css("transform", "rotate(90deg)");
-			accordionState[id] = true;
-		}else{
-			$("#" + id + " img").css("transform", "rotate(0)");
-			accordionState[id] = false;
-		}
-	});
+$(function() {
+    var last = $.cookie('goal-id-open');
+    if (last != null && checkIdIsValid(last)) { // if there exists opend accordion item
+
+        // mark all accordion items as closed
+        $("#accordion2 .collapse").removeClass('in');
+
+        // show the last opend accordion item
+        var idLast = "#" + last;
+        var $idLast = $(idLast);
+        if ($idLast.length > 0) {
+            $idLast.addClass("in");
+            $("a.accordion-toggle[href='#" + idLast + "'] > img").css("transform", "rotate(90deg)");
+        }
+    }
+
+    $('.accordion-toggle').on("click", function() {
+        var $this = $(this);
+        var bodyId = $(this).attr("href").substr(1);
+
+        // close all accordion items
+        $(".accordion-toggle").each(function() {
+            var j = $(this).attr('id');
+            $("#" + j + " " + "img").css("transform", "rotate(0)");
+        });
+
+        if (! $("#" + bodyId).hasClass("in")) { // the accordion item is not opened ?
+            // open the accordion item
+            $(this).children("img").css("transform", "rotate(90deg)");
+            var bodyId = $(this).attr("href").substr(1);
+            $.cookie('goal-id-open', bodyId);
+        }
+    });
 });
 
